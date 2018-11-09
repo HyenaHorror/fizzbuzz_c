db0df966eccaf23ccb5c0d2c0bd0155f006a2b5e
Rob Bird <robertbirdwv@gmail.com>
2018-11-08 20:51:47 -0500

    Fixed indentation.

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -4,14 +4,14 @@ int main()
 {
 	int i;
 	for (i = 0; i < 100; i = i + 1){
-			if(i % 15 == 0){
-				printf("FIZZ BUZZ\n");
-			} else if (i % 3 == 0){
-				printf("FIZZ\n");
-			} else if (i % 5 == 0){
-				printf("BUZZ\n");
-			} else {
-				printf("%d\n", i);
-			}
+		if(i % 15 == 0){
+			printf("FIZZ BUZZ\n");
+		} else if (i % 3 == 0){
+			printf("FIZZ\n");
+		} else if (i % 5 == 0){
+			printf("BUZZ\n");
+		} else {
+			printf("%d\n", i);
+		}
 	}
 }
