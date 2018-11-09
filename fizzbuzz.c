34e98c2c05811b0b0357f91287349b3107632a7f
Rob Bird <robertbirdwv@gmail.com>
2018-11-08 20:50:31 -0500

    Added script

---
+++ fizzbuzz.c
@@ -0,0 +1,17 @@
+#include <stdio.h> // include information about standard library
+
+int main()
+{
+	int i;
+	for (i = 0; i < 100; i = i + 1){
+			if(i % 15 == 0){
+				printf("FIZZ BUZZ\n");
+			} else if (i % 3 == 0){
+				printf("FIZZ\n");
+			} else if (i % 5 == 0){
+				printf("BUZZ\n");
+			} else {
+				printf("%d\n", i);
+			}
+	}
+}
